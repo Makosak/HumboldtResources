addb10ac44c4c67b0230a234a2a98a980dbc486b
Marynia <marynia.kolak@gmail.com>
2017-04-26 12:22:12 -0500

    updated categories

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -208,12 +208,13 @@
         //-----custom filters-----
         
         //Customized search by checkbox, connected with index.html
-        var type_column = "'Type'";
+        var type_column = "'NewType'";
         var searchType = type_column + " IN (-1,";
         if ( $("#cbType1").is(':checked')) searchType += "1,";
         if ( $("#cbType2").is(':checked')) searchType += "2,";
         if ( $("#cbType3").is(':checked')) searchType += "3,";
         if ( $("#cbType4").is(':checked')) searchType += "4,";
+        if ( $("#cbType5").is(':checked')) searchType += "5,";
 
         self.whereClause += " AND " + searchType.slice(0, searchType.length - 1) + ")";
 
